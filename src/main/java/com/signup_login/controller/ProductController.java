2f0b3c1926a2ac4be6d51e4c836214b9cff56eea
kimboseong <njunju77@nate.com>
2024-07-28 00:26:19 +0900

    추가: 접근불가페이지 설정

--- src/main/java/com/signup_login/controller/ProductController.java
+++ src/main/java/com/signup_login/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.signup_login.controller;
 
 import com.signup_login.model.User;
+import com.signup_login.model.UserRoleEnum;
 import com.signup_login.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,17 @@ public class ProductController {
         System.out.println("user.getUsername() = " + user.getUsername());
         System.out.println(user.getEmail());
         System.out.println(user.getRole());
+
+        return "redirect:/";
+    }
+
+    @Secured(UserRoleEnum.Authority.ADMIN)
+    @GetMapping("/products/secured")
+    public String getProductByAdmin(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
+        for (GrantedAuthority authority : userDetails.getAuthorities()) {
+            System.out.println("authority.getAuthority() = " + authority.getAuthority());
+        }
 
         return "redirect:/";
     }
