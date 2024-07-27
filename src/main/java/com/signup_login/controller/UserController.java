4fc252eabb81778b2b2eed506188e49147c5165c
kimboseong <njunju77@nate.com>
2024-07-27 14:55:37 +0900

    추가: 회원가입 메서드 추가

--- src/main/java/com/signup_login/controller/UserController.java
+++ src/main/java/com/signup_login/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.signup_login.controller;
 
+import com.signup_login.dto.SignupRequestDto;
+import com.signup_login.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
 public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user/login-page")
     public String loginPage() {
@@ -15,5 +22,11 @@ public class UserController {
     @GetMapping("/user/signup")
     public String signupPage(){
         return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto){
+        userService.signup(requestDto);
+        return "redirect:/user/login-page";
     }
 }
