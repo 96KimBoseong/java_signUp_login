e0f65526620ab38a713047fa931f648da44bc214
kimboseong <njunju77@nate.com>
2024-07-27 14:33:01 +0900

    추가: 유저 엔티티 추가

---
+++ src/main/java/com/signup_login/entity/User.java
@@ -0,0 +1,29 @@
+package com.signup_login.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+}
