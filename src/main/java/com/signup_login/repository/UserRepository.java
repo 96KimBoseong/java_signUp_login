ef99a18bee9940b1da4424e1b23f8b00b1cd102b
kimboseong <njunju77@nate.com>
2024-07-27 14:55:02 +0900

    추가: service / repository 생성

---
+++ src/main/java/com/signup_login/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.signup_login.repository;
+
+import com.signup_login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/signup_login/service/UserService.java
@@ -0,0 +1,8 @@
+package com.signup_login.service;
+
+import com.signup_login.dto.SignupRequestDto;
+
+public interface UserService {
+    public void signup(SignupRequestDto requestDto);
+
+}
