26151c11889c8b2ca4eef234bd569b3fd02d8109
kimboseong <njunju77@nate.com>
2024-07-27 20:55:09 +0900

    추가: 시큐리티 폼로그인 설정

--- src/main/java/com/signup_login/infra/config/WebSecurityConfig.java
+++ src/main/java/com/signup_login/infra/config/WebSecurityConfig.java
@@ -15,15 +15,30 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //csrf 설정
-        http.csrf((csrf)-> csrf.disable());
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable());
 
-        http.authorizeHttpRequests((authorizeHttpRequests)->
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()//리소스 접근허용
-                        .anyRequest().authenticated()//그 외 모든 요청 인증처리
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
-        http.formLogin(Customizer.withDefaults()); // 폼로그인 사용
+
+        // 로그인 사용
+        http.formLogin((formLogin) ->
+                formLogin
+                        // 로그인 View 제공 (GET /api/user/login-page)
+                        .loginPage("/api/user/login-page") // 커스텀 로그인 페이지 사용
+                        // 로그인 처리 (POST /api/user/login)
+                        .loginProcessingUrl("/api/user/login")
+                        // 로그인 처리 후 성공 시 URL
+                        .defaultSuccessUrl("/")
+                        // 로그인 처리 후 실패 시 URL
+                        .failureUrl("/api/user/login-page?error")
+                        .permitAll()
+        );
+
         return http.build();
     }
 }
