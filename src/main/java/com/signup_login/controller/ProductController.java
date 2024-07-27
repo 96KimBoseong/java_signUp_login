07d4be78792af8841e304be4373e9141bda971dc
kimboseong <njunju77@nate.com>
2024-07-28 00:42:45 +0900

    추가: validation 예외처리 글로벌핸들러에서 하는게 낫다 ~

--- src/main/java/com/signup_login/controller/ProductController.java
+++ src/main/java/com/signup_login/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.signup_login.controller;
 
+import com.signup_login.dto.ProductRequestDto;
 import com.signup_login.model.User;
 import com.signup_login.model.UserRoleEnum;
 import com.signup_login.security.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
@@ -35,5 +36,11 @@ public class ProductController {
         }
 
         return "redirect:/";
+    }
+
+    @PostMapping("/validation")
+    @ResponseBody
+    public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+        return requestDto;
     }
 }
--- src/main/java/com/signup_login/controller/UserController.java
+++ src/main/java/com/signup_login/controller/UserController.java
@@ -2,11 +2,17 @@ package com.signup_login.controller;
 
 import com.signup_login.dto.SignupRequestDto;
 import com.signup_login.service.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
@@ -25,8 +31,18 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto){
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
+
         return "redirect:/api/user/login-page";
     }
 
